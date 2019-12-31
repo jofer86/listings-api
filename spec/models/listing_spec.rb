aeed15ac111a715271e28b83bb6f60262d31634e
Jorge Fernando Rincón Figueroa <46115725+jofer86@users.noreply.github.com>
2019-12-31 14:07:53 -0600

    TDD for models, property_type property, with validations

--- spec/models/listing_spec.rb
+++ spec/models/listing_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
+describe '#listings' do
+  it 'should generate a valid factory' do
+    listing = build :listing
+    expect(listing).to be_valid
+  end
+
+  it 'should validate presence of property_type' do
+    listing = build :listing, property_type: ''
+    expect(listing).not_to be_valid
+    expect(listing.errors.messages[:property_type]).to include("can't be blank")
+  end
+
+  it 'should validate if the real state type is valid' do
+    listing = build :listing, property_type: 'bunker'
+    expect(listing).not_to be_valid
+    expect(listing.errors.messages[:property_type]).to include("#{listing[:property_type]} is not a valid property type")
+  end
 
 end
 
