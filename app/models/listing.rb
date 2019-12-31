8c65cdf8156ec6df53425a4c1c50aac2f15850ff
Jorge Fernando Rincón Figueroa <46115725+jofer86@users.noreply.github.com>
2019-12-31 16:23:53 -0600

    finish tests for all  integer properties of listings

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -4,4 +4,8 @@ class Listing < ApplicationRecord
   validates :property_type, presence: true, inclusion: { in: %w[Apartment House Warehouse],
                                                          message: '%{value} is not a valid property type' }
   validates :bedrooms, presence: true, numericality: { only_integer: true }
+  validates :bathrooms, presence: true, numericality: { only_integer: true }
+  validates :floor, presence: true, numericality: { only_integer: true }
+  validates :meters, presence: true, numericality: { only_integer: true }
+  validates :price, presence: true, numericality: { only_integer: true }
 end
--- spec/models/listing_spec.rb
+++ spec/models/listing_spec.rb
@@ -18,8 +18,8 @@ describe '#listings' do
     invalid_listing = build :listing, property_type: 'bunker'
     valid_listing = build :listing, property_type: 'Warehouse'
     expect(invalid_listing).not_to be_valid
-    expect(invalid_listing.errors.messages[:property_type]).to
-    include("#{invalid_listing[:property_type]} is not a valid property type")
+    expect(invalid_listing.errors.messages[:property_type]).to include
+      ("#{invalid_listing[:property_type]} is not a valid property type")
     expect(valid_listing).to be_valid
   end
 
@@ -34,6 +34,62 @@ describe '#listings' do
     valid_listing = build :listing, bedrooms: 2
     expect(invalid_listing).not_to be_valid
     expect(invalid_listing.errors.messages[:bedrooms]).to include('is not a number')
+    expect(valid_listing).to be_valid
+  end
+
+  it 'should validate the presence of the bathrooms property' do
+    listing = build :listing, bathrooms: ''
+    expect(listing).not_to be_valid
+    expect(listing.errors.messages[:bathrooms]).to include("can't be blank")
+  end
+
+  it 'should validate the bathrooms property to be an integer' do
+    invalid_listing = build :listing, bathrooms: 'string'
+    valid_listing = build :listing, bathrooms: 2
+    expect(invalid_listing).not_to be_valid
+    expect(invalid_listing.errors.messages[:bathrooms]).to include('is not a number')
+    expect(valid_listing).to be_valid
+  end
+
+    it 'should validate the presence of the floor property' do
+    listing = build :listing, floor: ''
+    expect(listing).not_to be_valid
+    expect(listing.errors.messages[:floor]).to include("can't be blank")
+  end
+
+  it 'should validate the floor property to be an integer' do
+    invalid_listing = build :listing, floor: 'string'
+    valid_listing = build :listing, floor: 2
+    expect(invalid_listing).not_to be_valid
+    expect(invalid_listing.errors.messages[:floor]).to include('is not a number')
+    expect(valid_listing).to be_valid
+  end
+
+  it 'should validate the presence of the meters property' do
+    listing = build :listing, meters: ''
+    expect(listing).not_to be_valid
+    expect(listing.errors.messages[:meters]).to include("can't be blank")
+  end
+
+  it 'should validate the meters property to be an integer' do
+    invalid_listing = build :listing, meters: 'string'
+    valid_listing = build :listing, meters: 2
+    expect(invalid_listing).not_to be_valid
+    expect(invalid_listing.errors.messages[:meters]).to include('is not a number')
+    expect(valid_listing).to be_valid
+  end
+
+  it 'should validate the presence of the price property' do
+    listing = build :listing, price: ''
+    expect(listing).not_to be_valid
+    expect(listing.errors.messages[:price]).to include("can't be blank")
+  end
+
+  it 'should validate the price property to be an integer' do
+    invalid_listing = build :listing, price: 'string'
+    valid_listing = build :listing, price: 2
+    expect(invalid_listing).not_to be_valid
+    expect(invalid_listing.errors.messages[:price]).to include('is not a number')
     expect(valid_listing).to be_valid
   end
 end
