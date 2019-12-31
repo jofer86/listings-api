293e785cb9d48cc1ef2b14b9aad91c8d43249265
Jorge Fernando Rincón Figueroa <46115725+jofer86@users.noreply.github.com>
2019-12-31 15:45:50 -0600

    added validations for bedrooms property with tests

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -3,4 +3,5 @@
 class Listing < ApplicationRecord
   validates :property_type, presence: true, inclusion: { in: %w[Apartment House Warehouse],
                                                          message: '%{value} is not a valid property type' }
+  validates :bedrooms, presence: true, numericality: { only_integer: true }
 end
--- spec/models/listing_spec.rb
+++ spec/models/listing_spec.rb
@@ -15,9 +15,25 @@ describe '#listings' do
   end
 
   it 'should validate if the real state type is valid' do
-    listing = build :listing, property_type: 'bunker'
-    expect(listing).not_to be_valid
-    expect(listing.errors.messages[:property_type]).to include("#{listing[:property_type]} is not a valid property type")
+    invalid_listing = build :listing, property_type: 'bunker'
+    valid_listing = build :listing, property_type: 'Warehouse'
+    expect(invalid_listing).not_to be_valid
+    expect(invalid_listing.errors.messages[:property_type]).to include("#{invalid_listing[:property_type]} is not a valid property type")
+    expect(valid_listing).to be_valid
   end
-  
+
+  it 'should validate the presence of the bedrooms property' do
+    listing = build :listing, bedrooms: ''
+    expect(listing).not_to be_valid
+    expect(listing.errors.messages[:bedrooms]).to include("can't be blank")
+  end
+
+  it 'should validate the bedrooms property to be an integer' do
+    invalid_listing = build :listing, bedrooms: "string"
+    valid_listing = build :listing, bedrooms: 2
+    expect(invalid_listing).not_to be_valid
+    expect(invalid_listing.errors.messages[:bedrooms]).to include("is not a number")
+    expect(valid_listing).to be_valid
+  end
+
 end
