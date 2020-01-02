17add3944ee5200ccea29f654424d8e39aa7f981
Jorge Fernando Rincón Figueroa <46115725+jofer86@users.noreply.github.com>
2020-01-02 11:20:55 -0600

    linting on errors

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -2,12 +2,12 @@
 
 class Listing < ApplicationRecord
   validates :property_type, presence: true, inclusion: { in: %w[Apartment House Warehouse],
-  message: '%{value} is not a valid property type' }
+                                                         message: '%<value> is not a valid property type' }
   validates :bedrooms, presence: true, numericality: { only_integer: true }
   validates :bathrooms, presence: true, numericality: { only_integer: true }
   validates :floor, presence: true, numericality: { only_integer: true }
   validates :meters, presence: true, numericality: { only_integer: true }
   validates :price, presence: true, numericality: { only_integer: true }
-  validates :description, presence: true, length: { minimum: 50,
-  message: 'Description is too short, minimum characters allowed are 50'}
+  validates :description, presence: true,
+                          length: { minimum: 50, message: 'Description is too short' }
 end
