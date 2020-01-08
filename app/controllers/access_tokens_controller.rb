c26910373b22300bf534c07cd4c14071dd7527eb
Jorge Fernando Rincón Figueroa <46115725+jofer86@users.noreply.github.com>
2020-01-07 20:20:41 -0600

    linting

--- app/controllers/access_tokens_controller.rb
+++ app/controllers/access_tokens_controller.rb
@@ -1,11 +1,13 @@
+# frozen_string_literal: true
+
 class AccessTokensController < ApplicationController
   def create
     error = {
-      "status" => "401",
-      "source" => { "pointer" => "/code" },
-      "title" =>  "Authentication code is invalid",
-      "detail" => "A valid code must be provided in order to exchange it for token."
+      'status' => '401',
+      'source' => { 'pointer' => '/code' },
+      'title' => 'Authentication code is invalid',
+      'detail' => 'A valid code must be provided in order to exchange it for token.'
     }
-    render json: { "errors": [ error ] }, status: 401
+    render json: { "errors": [error] }, status: 401
   end
 end
