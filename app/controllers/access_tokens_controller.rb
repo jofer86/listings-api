19a192309feb682a16884d0373ff40ba766deb83
Jorge Fernando Rincón Figueroa <46115725+jofer86@users.noreply.github.com>
2020-01-07 20:39:11 -0600

    moved authentication error handling to the applicationcontroller

--- app/controllers/access_tokens_controller.rb
+++ app/controllers/access_tokens_controller.rb
@@ -1,22 +1,8 @@
 # frozen_string_literal: true
 
-class AccessTokensController < ApplicationController
-  rescue_from UserAuthenticator::AuthenticationError, with: :authentication_error
+class AccessTokensController < ApplicationController  
   def create
     authenticator = UserAuthenticator.new(params[:code])
     authenticator.perform
   end
-
-  private
-
-  def authentication_error
-    error = {
-      'status' => '401',
-      'source' => { 'pointer' => '/code' },
-      'title' => 'Authentication code is invalid',
-      'detail' => 'A valid code must be provided in order to exchange it for token.'
-    }
-    render json: { "errors": [error] }, status: 401
-  end
-
 end
