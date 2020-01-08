2542ec9795e26b426ca53c633e151ecb5edd8b80
Jorge Fernando Rincón Figueroa <46115725+jofer86@users.noreply.github.com>
2020-01-07 20:37:10 -0600

    refactor code in acesstokens controller add rescue_from block

--- app/controllers/access_tokens_controller.rb
+++ app/controllers/access_tokens_controller.rb
@@ -1,7 +1,15 @@
 # frozen_string_literal: true
 
 class AccessTokensController < ApplicationController
+  rescue_from UserAuthenticator::AuthenticationError, with: :authentication_error
   def create
+    authenticator = UserAuthenticator.new(params[:code])
+    authenticator.perform
+  end
+
+  private
+
+  def authentication_error
     error = {
       'status' => '401',
       'source' => { 'pointer' => '/code' },
@@ -10,4 +18,5 @@ class AccessTokensController < ApplicationController
     }
     render json: { "errors": [error] }, status: 401
   end
+
 end
